7eac0c2f10a386a745b76fc01fe6d3f9983e824f
Oleksandr Kotliarov <de.fr09n@gmail.com>
2023-07-16 18:26:34 +0300

    -

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   // Config setup
   const config = app.get(ConfigService);
-  const PORT = config.get<number>('API_PORT') || 3000;
+  const PORT = process.env.PORT || 5000;
 
   // CORS setup
   const CLIENT_URL = config.get<string>('CLIENT_URL');
